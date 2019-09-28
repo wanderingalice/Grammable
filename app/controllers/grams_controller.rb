021a84b9b54b09a399ce658eed8803e40c9164c6
Alice Duff <duff.alice@gmail.com>
2019-09-28 22:12:35 +0000

    added tests and destroy function

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,15 +1,21 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
+
+  end
+
   def index
 
   end
 
   def edit
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      return render_not_found if @gram.blank?
-    end
+    return render_not_found if @gram.blank?
   end
 
   def update
