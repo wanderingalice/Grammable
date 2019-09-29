247af24833a70335ff73e81011a52cb7836fdb93
Alice Duff <duff.alice@gmail.com>
2019-09-29 02:09:39 +0000

    added test coverage for picture

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -152,7 +152,12 @@ RSpec.describe GramsController, type: :controller do
        user = FactoryBot.create(:user)
       sign_in user
 
-      post :create, params: { gram: { message: 'Hello!' } }
+      post :create, params: {
+        gram: {
+          message: 'Hello!',
+          picture: fixture_file_upload("/picture.jpg", 'image/jpg')
+        }
+      }
       expect(response).to redirect_to root_path
 
       gram = Gram.last
--- spec/factories.rb
+++ spec/factories.rb
@@ -9,6 +9,7 @@ FactoryBot.define do
 
   factory :gram do
     message { "hello" }
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.jpg').to_s, 'image/jpg') }
     association :user
   end
 end
